619e926bd6daf7764a07adf9a2e9dabe8804fa00
Lucca Scarano <lucca.scarano@hotmail.com>
2020-06-16 12:52:49 -0300

    http methods (get, post, put, delete)

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,48 @@ const express = require('express');
 const app = express();
 
 app.get('/projects', (request, response) => {
-    return response.send("Hello World");
+    return response.json({
+        "projects": [
+            'Sapphire',
+            'GoStack',
+            'Projeto 3'
+        ]
+    });
 });
 
-app.listen(3333); // -> set port to node.js Example - localhost:3333
+app.post('/projects', (request, response) => {
+    return response.json({
+        "projects": [
+            'Sapphire',
+            'GoStack',
+            'Projeto 3',
+            'Projeto 4'
+        ]
+    })
+})
+
+app.put('/projects/:id', (request, response) => {
+    return response.json({
+        "projects": [
+            'Sapphire 2.0',
+            'GoStack',
+            'Projeto 3',
+            'Projeto 4'
+        ]
+    })
+})
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json({
+        "projects": [
+            'Sapphire 2.0',
+            'GoStack',
+            'Projeto 3',
+        ]
+    })
+})
+
+app.listen(3333, () => {
+    console.log("🚀 Back-end Started");
+}); // -> set port to node.js Example - localhost:3333
 
