2fdb4470a172332f64ac10ff9be73765210b28e9
Lucca Scarano <lucca.scarano@hotmail.com>
2020-06-22 12:51:05 -0300

    backend requests from frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const {uuid} = require('uuidv4');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 const projects = [];
@@ -23,11 +25,9 @@ function logRequests(request, response, next){
     const {method, url} = request;
     const logLabel = `[${method.toUpperCase()} ${url}]`
 
-    console.time(logRequests);
+    console.log(logLabel);
 
     next();
-
-    console.timeEnd(logRequests);
 }
 
 app.use(logRequests);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
+import "./App.css";
+import api from "./services/api"
 
 export default function App(){
-    const [projects, setProjects] = useState(['desenvolvimento de app', 'Front-end web']);
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('/projects')
+            .then(response => {
+                setProjects(response.data);
+            })
+    }, []);
 
     //useState retorna array em duas posiçoes:
     //1. a variável com seu valor inicial
     //2. uma função para alterar seu valor
 
-    function handleAddProject(){
-        // projects.push(`Novo projeto ${Date.now()}`); -> não respeita a imutabilidade, altera diretamente o valor da variável
-        
-        setProjects([...projects, `Novo projeto ${Date.now()}`]); //-> Altera de forma indireta.
+    async function handleAddProject(){      
+        const response_async = await api.post('/projects', {
+            title: `Projeto Mobile ${Date.now()}`,
+            owner: 'Lucca'
+        });
 
-        console.log(projects);
+        const project = response_async.data;
+        setProjects([...projects, project])
     }
 
     return (
         <>
-            <Header title="Projeto 1">
-                <ul>
-                    <li>alou</li>
-                    <li>alou2</li>
-                </ul>    
-            </Header>
-            <Header title="Projeto 2"/>
-
             <Header title="Projects">
                 <ul>
-                    {projects.map(project => <li key = {project}>{project}</li>)}
+                    {projects.map(project => <li key = {project.id}>{project.title}</li>)}
                 </ul>
 
                 <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
