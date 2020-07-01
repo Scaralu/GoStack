2631998e045408225e940d28587d17e0b74418da
Lucca Scarano <lucca.scarano@hotmail.com>
2020-07-01 09:03:29 -0300

    eslint setup and json configuration

--- backend/nodejs_project/src/server.ts
+++ backend/nodejs_project/src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.json({message: 'Hello GoStack'})
-})
-
+  return res.json({message: 'Hello GoStack'})
+});
 
 app.listen(3333, ( )=> {
-    console.log("🍖  Robust server started on port 3333")
+	console.log("🍖  Robust server started on port 3333")
 });
