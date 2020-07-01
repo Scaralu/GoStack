3ceb0d35e94da6d0728cd3d18576e78fb1f8ba13
Lucca Scarano <lucca.scarano@hotmail.com>
2020-07-01 07:51:12 -0300

    initial setup and folder organization

---
+++ backend/nodejs_project/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.json({message: 'Hello GoStack'})
+})
+
+
+app.listen(3333, ( )=> {
+    console.log("🍖  Robust server started on port 3333")
+});
